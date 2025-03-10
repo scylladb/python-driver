scylladb/python-driver

commit 3ce7d7aaaedd10a709e67e5d3e565035c1c736e3

PYTHON-1364 Fix ssl.wrap_socket errors (from eventlet) for Python 3.12 (#1181)

cassandra/cluster.py
@@ -116,8 +116,12 @@ except ImportError:
 def _is_gevent_monkey_patched():
     if 'gevent.monkey' not in sys.modules:
         return False
-    import gevent.socket
-    return socket.socket is gevent.socket.socket
+    try:
+        import eventlet.patcher
+        return eventlet.patcher.is_monkey_patched('socket')
+    # Another case related to PYTHON-1364
+    except AttributeError:
+        return False
 
 def _try_gevent_import():
     if _is_gevent_monkey_patched():

commit ab398dbaad8b20b5d9374c71b868f1830efd4eda

cluster: fix _is_gevent_monkey_patched
Commit 3ce7d7aaaedd10a709e67e5d3e565035c1c736e3 introduced regression
when instead of geven it is trying to import eventlet

cassandra/cluster.py
@@ -117,10 +117,9 @@ def _is_gevent_monkey_patched():
     if 'gevent.monkey' not in sys.modules:
         return False
     try:
-        import eventlet.patcher
-        return eventlet.patcher.is_monkey_patched('socket')
-    # Another case related to PYTHON-1364
-    except AttributeError:
+        import gevent.socket
+        return socket.socket is gevent.socket.socket    # Another case related to PYTHON-1364
+    except (AttributeError, ImportError):
         return False
 
 def _try_gevent_import():