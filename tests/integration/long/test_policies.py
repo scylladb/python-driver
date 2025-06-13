61668dedc6571124a0463d018558db9309c81baa
Dmitry Kropachev <dmitry.kropachev@gmail.com>
2025-06-13 13:35:31 -0400

    feat(cluster): inject shard reconnection policy
    Inject shard reconnection policy into cluster, session, connection and host pool.
    Drop pending connections tracking logic, since policy does that.
    Fix some tests that mocks Cluster, session, connection or host pool.

--- tests/integration/long/test_policies.py
+++ tests/integration/long/test_policies.py
@@ -11,16 +11,22 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+import os
+import time
 import unittest
+from typing import Optional
 
 from cassandra import ConsistencyLevel, Unavailable
-from cassandra.cluster import ExecutionProfile, EXEC_PROFILE_DEFAULT
+from cassandra.cluster import ExecutionProfile, EXEC_PROFILE_DEFAULT, Session
+from cassandra.policies import LimitedConcurrencyShardConnectionBackoffPolicy, ShardConnectionBackoffScope, \
+    ConstantReconnectionPolicy, ShardConnectionBackoffPolicy, NoDelayShardConnectionBackoffPolicy
+from cassandra.shard_info import _ShardingInfo
 
 from tests.integration import use_cluster, get_cluster, get_node, TestCluster
 
 
 def setup_module():
+    os.environ['SCYLLA_EXT_OPTS'] = "--smp 4"
     use_cluster('test_cluster', [4])
 
 
@@ -65,3 +71,127 @@ class RetryPolicyTests(unittest.TestCase):
         self.assertEqual(exception.consistency, ConsistencyLevel.SERIAL)
         self.assertEqual(exception.required_replicas, 2)
         self.assertEqual(exception.alive_replicas, 1)
+
+
+class ShardBackoffPolicyTests(unittest.TestCase):
+    @classmethod
+    def tearDownClass(cls):
+        cluster = get_cluster()
+        cluster.start(wait_for_binary_proto=True, wait_other_notice=True)  # make sure other nodes are restarted
+
+    def test_limited_concurrency_1_connection_per_cluster(self):
+        self._test_backoff(
+            LimitedConcurrencyShardConnectionBackoffPolicy(
+                backoff_policy=ConstantReconnectionPolicy(0.1),
+                max_concurrent=1,
+                scope=ShardConnectionBackoffScope.Cluster,
+            )
+        )
+
+    def test_limited_concurrency_2_connection_per_cluster(self):
+        self._test_backoff(
+            LimitedConcurrencyShardConnectionBackoffPolicy(
+                backoff_policy=ConstantReconnectionPolicy(0.1),
+                max_concurrent=2,
+                scope=ShardConnectionBackoffScope.Cluster,
+            )
+        )
+
+    def test_limited_concurrency_1_connection_per_host(self):
+        self._test_backoff(
+            LimitedConcurrencyShardConnectionBackoffPolicy(
+                backoff_policy=ConstantReconnectionPolicy(0.1),
+                max_concurrent=1,
+                scope=ShardConnectionBackoffScope.Host,
+            )
+        )
+
+    def test_limited_concurrency_2_connection_per_host(self):
+        self._test_backoff(
+            LimitedConcurrencyShardConnectionBackoffPolicy(
+                backoff_policy=ConstantReconnectionPolicy(0.1),
+                max_concurrent=1,
+                scope=ShardConnectionBackoffScope.Host,
+            )
+        )
+
+    def test_no_delay(self):
+        self._test_backoff(NoDelayShardConnectionBackoffPolicy())
+
+    def _test_backoff(self, shard_connection_backoff_policy: ShardConnectionBackoffPolicy):
+        backoff_policy = None
+        if isinstance(shard_connection_backoff_policy, LimitedConcurrencyShardConnectionBackoffPolicy):
+            backoff_policy = shard_connection_backoff_policy.backoff_policy
+
+        cluster = TestCluster(
+            shard_connection_backoff_policy=shard_connection_backoff_policy,
+            reconnection_policy=ConstantReconnectionPolicy(0),
+        )
+        session = cluster.connect()
+        sharding_info = get_sharding_info(session)
+
+        # even if backoff is set and there is no sharding info
+        # behavior should be the same as if there is no backoff policy
+        if not backoff_policy or not sharding_info:
+            time.sleep(2)
+            expected_connections = 1
+            if sharding_info:
+                expected_connections = sharding_info.shards_count
+            for host_id, connections_count in get_connections_per_host(session).items():
+                self.assertEqual(connections_count, expected_connections)
+            return
+
+        sleep_time = 0
+        schedule = backoff_policy.new_schedule()
+        # Calculate total time it will need to establish all connections
+        if shard_connection_backoff_policy.scope == ShardConnectionBackoffScope.Cluster:
+            for _ in session.hosts:
+                for _ in range(sharding_info.shards_count - 1):
+                    sleep_time += next(schedule)
+            sleep_time /= shard_connection_backoff_policy.max_concurrent
+        elif shard_connection_backoff_policy.scope == ShardConnectionBackoffScope.Host:
+            for _ in range(sharding_info.shards_count - 1):
+                sleep_time += next(schedule)
+            sleep_time /= shard_connection_backoff_policy.max_concurrent
+        else:
+            raise ValueError("Unknown scope {}".format(shard_connection_backoff_policy.scope))
+
+        time.sleep(sleep_time / 2)
+        self.assertFalse(
+            is_connection_filled(shard_connection_backoff_policy.scope, session, sharding_info.shards_count))
+        time.sleep(sleep_time / 2 + 1)
+        self.assertTrue(
+            is_connection_filled(shard_connection_backoff_policy.scope, session, sharding_info.shards_count))
+
+
+def is_connection_filled(scope: ShardConnectionBackoffScope, session: Session, shards_count: int) -> bool:
+    if scope == ShardConnectionBackoffScope.Cluster:
+        expected_connections = shards_count * len(session.hosts)
+        total_connections = sum(get_connections_per_host(session).values())
+        return expected_connections == total_connections
+    elif scope == ShardConnectionBackoffScope.Host:
+        expected_connections_per_host = shards_count
+        for connections_count in get_connections_per_host(session).values():
+            if connections_count < expected_connections_per_host:
+                return False
+            if connections_count == expected_connections_per_host:
+                continue
+            assert False, "Expected {} or less connections but got {}".format(expected_connections_per_host,
+                                                                              connections_count)
+        return True
+    else:
+        raise ValueError("Unknown scope {}".format(scope))
+
+
+def get_connections_per_host(session: Session) -> dict[str, int]:
+    host_connections = {}
+    for host, pool in session._pools.items():
+        host_connections[host.host_id] = len(pool._connections)
+    return host_connections
+
+
+def get_sharding_info(session: Session) -> Optional[_ShardingInfo]:
+    for host in session.hosts:
+        if host.sharding_info:
+            return host.sharding_info
+    return None
