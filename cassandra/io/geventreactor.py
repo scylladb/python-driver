a3a0bbd5e9213d1feda61be9d2ce684f796032c6
Dmitry Kropachev <dmitry.kropachev@gmail.com>
2026-02-14 07:58:31 -0400

    Fix GeventConnection close() race causing EBADF errors (#614)
    GeventConnection.close() uses kill(block=False) which schedules
    GreenletExit for the next yield point, then closes the socket
    immediately. If close() is called from a non-gevent thread, the
    greenlet may still be mid-I/O when the socket is closed, causing
    EBADF.
    
    - Add is_closed/is_defunct guards in handle_read() and handle_write()
      error paths to silently exit during shutdown
    - Add GreenletExit exception handling in both I/O handlers for
      graceful greenlet termination (matching eventletreactor pattern)
    - Set last_error in close() when connected_event is not yet set to
      prevent factory() from returning a dead connection
    - Set last_error on server-initiated close (EOF) in handle_read()

--- cassandra/io/geventreactor.py
+++ cassandra/io/geventreactor.py
@@ -16,6 +16,7 @@ import gevent.event
 from gevent.queue import Queue
 from gevent import socket
 import gevent.ssl
+from greenlet import GreenletExit
 
 import logging
 import time
@@ -98,7 +99,10 @@ class GeventConnection(Connection):
             msg = "Connection to %s was closed" % self.endpoint
             if self.last_error:
                 msg += ": %s" % (self.last_error,)
-            self.error_all_requests(ConnectionShutdown(msg))
+            shutdown_exc = ConnectionShutdown(msg)
+            self.error_all_requests(shutdown_exc)
+            if not self.connected_event.is_set():
+                self.last_error = shutdown_exc
             # don't leave in-progress operations hanging
             self.connected_event.set()
 
@@ -112,8 +116,12 @@ class GeventConnection(Connection):
                 next_msg = self._write_queue.get()
                 self._socket.sendall(next_msg)
             except socket.error as err:
+                if self.is_closed or self.is_defunct:
+                    return
                 log.debug("Exception in send for %s: %s", self, err)
                 self.defunct(err)
+                return
+            except GreenletExit:
                 return
 
     def handle_read(self):
@@ -122,14 +130,20 @@ class GeventConnection(Connection):
                 buf = self._socket.recv(self.in_buffer_size)
                 self._iobuf.write(buf)
             except socket.error as err:
+                if self.is_closed or self.is_defunct:
+                    return
                 log.debug("Exception in read for %s: %s", self, err)
                 self.defunct(err)
                 return  # leave the read loop
+            except GreenletExit:
+                return
 
             if buf and self._iobuf.tell():
                 self.process_io_buffer()
             else:
                 log.debug("Connection %s closed by server", self)
+                self.last_error = ConnectionShutdown(
+                    "Connection to %s was closed by server" % self.endpoint)
                 self.close()
                 return
 
