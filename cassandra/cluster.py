f2a78d5b92e7c002e1503d52ec1fd0e8805383f1
Yaniv Michael Kaul <yaniv.kaul@scylladb.com>
2026-04-03 19:41:18 +0300

    remove: redundant Python version checks that are always True/False on 3.9+
    - cluster.py: remove 'if sys.version_info >= 3.7' guard around eventlet
      check (always true since we require Python 3.9+).  Update error message.
    - test_insights.py: remove 'if sys.version_info > (3,)' guard around
      namespace suffix (always true).
    - test_row_factories.py: remove NAMEDTUPLE_CREATION_BUG flag and
      Python 3.0-3.6 warning path (bug was fixed in Python 3.7).

--- cassandra/cluster.py
+++ cassandra/cluster.py
@@ -1544,7 +1544,7 @@ class Cluster(object):
         Create a ThreadPoolExecutor for the cluster. In most cases, the built-in
         `concurrent.futures.ThreadPoolExecutor` is used.
 
-        Python 3.7+ and Eventlet cause the `concurrent.futures.ThreadPoolExecutor`
+        Eventlet causes the `concurrent.futures.ThreadPoolExecutor`
         to hang indefinitely. In that case, the user needs to have the `futurist`
         package so we can use the `futurist.GreenThreadPoolExecutor` class instead.
 
@@ -1552,26 +1552,25 @@ class Cluster(object):
         :return: A ThreadPoolExecutor instance.
         """
         tpe_class = ThreadPoolExecutor
-        if sys.version_info[0] >= 3 and sys.version_info[1] >= 7:
-            try:
-                from cassandra.io.eventletreactor import EventletConnection
-                is_eventlet = issubclass(self.connection_class, EventletConnection)
-            except:
-                # Eventlet is not available or can't be detected
-                return tpe_class(**kwargs)
+        try:
+            from cassandra.io.eventletreactor import EventletConnection
+            is_eventlet = issubclass(self.connection_class, EventletConnection)
+        except:
+            # Eventlet is not available or can't be detected
+            return tpe_class(**kwargs)
 
-            if is_eventlet:
-                try:
-                    from futurist import GreenThreadPoolExecutor
-                    tpe_class = GreenThreadPoolExecutor
-                except ImportError:
-                    # futurist is not available
-                    raise ImportError(
-                        ("Python 3.7+ and Eventlet cause the `concurrent.futures.ThreadPoolExecutor` "
-                         "to hang indefinitely. If you want to use the Eventlet reactor, you "
-                         "need to install the `futurist` package to allow the driver to use "
-                         "the GreenThreadPoolExecutor. See https://github.com/eventlet/eventlet/issues/508 "
-                         "for more details."))
+        if is_eventlet:
+            try:
+                from futurist import GreenThreadPoolExecutor
+                tpe_class = GreenThreadPoolExecutor
+            except ImportError:
+                # futurist is not available
+                raise ImportError(
+                    ("Eventlet causes the `concurrent.futures.ThreadPoolExecutor` "
+                     "to hang indefinitely. If you want to use the Eventlet reactor, you "
+                     "need to install the `futurist` package to allow the driver to use "
+                     "the GreenThreadPoolExecutor. See https://github.com/eventlet/eventlet/issues/508 "
+                     "for more details."))
 
         return tpe_class(**kwargs)
 
